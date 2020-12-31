7685c9614903519ccb228e8bc91a6e72d1a2f906
choon <chachooon@gmail.com>
2020-12-31 20:08:48 +0900

    JWT Module part Two

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,7 +42,9 @@ import { JwtModule } from './jwt/jwt.module';
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    JwtModule.forRoot(),
+    JwtModule.forRoot({
+      privateKey: process.env.PRIVATE_KEY,
+    }),
     UsersModule,
     CommonModule,
   ],
